fd052d75a1f68fea736284537fb9f202ee341bf6
Frank Denis <github@pureftpd.org>
2015-02-16 20:42:21 +0100

    No need for temporary buffers and moving bytes around for crypto_box

--- crypto.cpp
+++ crypto.cpp
@@ -14,37 +14,16 @@ Crypto::Crypto()
 
 int rawencrypt(char encrypted[], const uint8_t pk[], const uint8_t sk[], const char nonce[], const char plain[], int length)
 {
-    uint8_t* temp_encrypted = new uint8_t[length+crypto_box_ZEROBYTES];
-    uint8_t* temp_plain = new uint8_t[length+crypto_box_ZEROBYTES];
-
-    memset(temp_encrypted, '\0', crypto_box_BOXZEROBYTES);
-    memset(temp_plain, '\0', crypto_box_ZEROBYTES);
-    memcpy(temp_plain + crypto_box_ZEROBYTES, plain, length);
-
-    if (crypto_box(temp_encrypted, temp_plain, crypto_box_ZEROBYTES + length, (uint8_t*)nonce, pk, sk))
+    if (crypto_box_easy((uint8_t*)encrypted, (uint8_t*)plain, length, (uint8_t*)nonce, pk, sk))
         throw std::runtime_error("Crypto encrypt: Encryption failed");
-
-    memcpy(encrypted, temp_encrypted + crypto_box_BOXZEROBYTES, crypto_box_BOXZEROBYTES+length);
-    delete[] temp_plain;
-    delete[] temp_encrypted;
 
     return length;
 }
 
 int rawdecrypt(char plain[], const uint8_t pk[], const uint8_t sk[], const char nonce[], const char encrypted[], int length)
 {
-    uint8_t* temp_encrypted = new uint8_t[length+crypto_box_ZEROBYTES];
-    uint8_t* temp_plain = new uint8_t[length+crypto_box_ZEROBYTES];
-
-    memset(temp_encrypted, '\0', crypto_box_BOXZEROBYTES);
-    memcpy(temp_encrypted + crypto_box_BOXZEROBYTES, encrypted, length);
-
-    if (crypto_box_open(temp_plain, temp_encrypted, crypto_box_BOXZEROBYTES + length, (uint8_t*)nonce, pk, sk))
+    if (crypto_box_open_easy((uint8_t*)plain, (uint8_t*)encrypted, length, (uint8_t*)nonce, pk, sk))
         throw std::runtime_error("Crypto decrypt: Invalid or forged cyphertext");
-
-    memcpy(plain, temp_plain + crypto_box_ZEROBYTES, length);
-    delete[] temp_plain;
-    delete[] temp_encrypted;
 
     return length;
 }
@@ -76,7 +55,7 @@ void Crypto::encryptPacket(NetPacket& packet, const Server &s, const PublicKey& 
         return;
     size_t encryptedsize = packet.data.size()+crypto_box_NONCEBYTES+crypto_box_BOXZEROBYTES;
     std::vector<char> encrypted(encryptedsize);
-    randombytes(&encrypted[0], crypto_box_NONCEBYTES);
+    randombytes((uint8_t*)&encrypted[0], crypto_box_NONCEBYTES);
     rawencrypt(&encrypted[crypto_box_NONCEBYTES], &remoteKey[0], &s.getSecretKey()[0], &encrypted[0],
             &packet.data[0], packet.data.size());
     packet.data = encrypted;
@@ -102,7 +81,7 @@ void Crypto::encrypt(std::vector<char>& data, const Server& s, const PublicKey &
         return;
     size_t encryptedsize = data.size()+crypto_box_NONCEBYTES+crypto_box_BOXZEROBYTES;
     std::vector<char> encrypted(encryptedsize);
-    randombytes(&encrypted[0], crypto_box_NONCEBYTES);
+    randombytes((uint8_t*)&encrypted[0], crypto_box_NONCEBYTES);
     rawencrypt(&encrypted[crypto_box_NONCEBYTES], &remoteKey[0], &s.getSecretKey()[0], &encrypted[0],
             &data[0], data.size());
     data = encrypted;

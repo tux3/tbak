3afca9a0cd300646629fb6ff2cc24df2a2ba4843
tux3 <barrdetwix@gmail.com>
2015-10-21 18:36:09 +0200

    Replace zlib by libbrotli

--- compression.cpp
+++ compression.cpp
@@ -1,54 +1,54 @@
 #include "compression.h"
 #include <zlib.h>
+#include <brotli/dec/decode.h>
+#include <brotli/enc/encode.h>
+#include <stdexcept>
 
-Compression::Compression()
+using namespace std;
+using namespace brotli;
+
+class BrotliVecOut : public brotli::BrotliOut
 {
+public:
+    BrotliVecOut(std::vector<char>& out) : out{out} {}
+    bool Write(const void* buf, size_t n)
+    {
+        out.insert(out.end(), (char*)buf, (char*)buf+n);
+        return true;
+    }
 
-}
+private:
+    std::vector<char>& out;
+};
 
 std::vector<char> Compression::deflate(const std::vector<char>& in)
 {
     std::vector<char> out;
-    const size_t chunk = chunksize;
-    unsigned char buf[chunk];
-    z_stream strm;
-    strm.zalloc = Z_NULL;
-    strm.zfree  = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.next_in = (unsigned char *) in.data();
-    strm.avail_in = in.size();
-    deflateInit2(&strm, 5, Z_DEFLATED,
-                window, memLevel, Z_DEFAULT_STRATEGY);
-    do {
-        strm.avail_out = chunk;
-        strm.next_out = buf;
-        ::deflate(&strm, Z_FINISH);
-        int have = chunk - strm.avail_out;
-        out.insert(end(out), buf, buf+have);
-    } while (strm.avail_out == 0);
-    deflateEnd (& strm);
+    BrotliParams params;
+    params.quality = 6;
+    BrotliMemIn brotliIn(in.data(), in.size());
+    BrotliVecOut brotliOut(out);
+    if (!BrotliCompress(params, &brotliIn, &brotliOut))
+        throw std::runtime_error("Failed to compress");
     return out;
 }
 
 std::vector<char> Compression::inflate(const std::vector<char>& in)
 {
     std::vector<char> out;
-    const size_t chunk = chunksize;
-    unsigned char buf[chunk];
-    z_stream strm;
-    strm.zalloc = Z_NULL;
-    strm.zfree  = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.next_in = (unsigned char *) in.data();
-    strm.avail_in = in.size();
-    inflateInit2(&strm, window);
-    do {
-            strm.avail_out = chunk;
-            strm.next_out = buf;
-            ::inflate(&strm, Z_FINISH);
-            int have = chunk - strm.avail_out;
-            out.insert(end(out), buf, buf+have);
-    } while (strm.avail_out == 0);
-    inflateEnd (& strm);
+    out.reserve(in.size());
+    BrotliMemInput brotliMemIn;
+    BrotliInput brotliIn = BrotliInitMemInput((uint8_t*)in.data(), in.size(), &brotliMemIn);
+    BrotliOutput brotliOut;
+    brotliOut.data_ = &out;
+    brotliOut.cb_ = [](void* _data, const uint8_t* buf, size_t count) -> int
+    {
+        vector<char>* data = static_cast<vector<char>*>(_data);
+        data->insert(data->end(), buf, buf+count);
+        return count;
+    };
+
+    if (!BrotliDecompress(brotliIn, brotliOut))
+        throw std::runtime_error("Failed to decompress");
     return out;
 }
--- compression.h
+++ compression.h
@@ -7,15 +7,10 @@
 class Compression
 {
 public:
-    Compression();
+    Compression() = delete;
 
     static std::vector<char> deflate(const std::vector<char>& in);
     static std::vector<char> inflate(const std::vector<char>& in);
-
-private:
-    static const int window = -15;
-    static const size_t chunksize = 4096;
-    static const unsigned memLevel = 9;
 };
 
 #endif // COMPRESSION_H
